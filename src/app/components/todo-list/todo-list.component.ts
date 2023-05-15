f66875205b2c2fdcbbafb114032182a67500228f
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-05-15 13:19:30 +0200

    asdas

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -8,11 +8,18 @@ import { Todo } from 'src/app/models/todo';
 })
 export class TodoListComponent {
 
-  @Input() todoArray?: Todo[] = [];      //default, questa proprieta puo essere riempita tramite l' HTML (input)
+  // @Input() todoArray?: Todo[] = [];      //default, questa proprieta puo essere riempita tramite l' HTML (input)
+
+  _todoArray: Todo[] = [];
+
+  @Input() set todoArray(value: Todo[]){
+    this._todoArray= value;
+    this._todoArray.sort((todo1,todo2)=> todo2.priority - todo1.priority)
+  }
 
   deleteTodo(todoToDelete: Todo){
     console.log('Devo cancellare',todoToDelete.title);
-    this.todoArray= this.todoArray?.filter(t => t.title !== todoToDelete.title); // La Filter() dice cosa tenere; tieni tutti i todo che sono diversi dal TODO.title che è arrivato
+    this.todoArray= this._todoArray?.filter(t => t.title !== todoToDelete.title); // La Filter() dice cosa tenere; tieni tutti i todo che sono diversi dal TODO.title che è arrivato
   }
 
 }
